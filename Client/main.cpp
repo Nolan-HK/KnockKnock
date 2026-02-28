ea15f41d0f131435d0e4b4128ffbf559dfcb9cea
Nolan <62446114+Nolan-HK@users.noreply.github.com>
2026-02-28 19:57:00 +0800

    Add files via upload

---
+++ Client/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "login/login.h"
+#include "base_widget/MainWindow.h"
+#include <QtWidgets/QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    LoginBase login;
+    QQ mainwindow;
+    login.show();
+    QObject::connect(&login, &LoginBase::signalLoginSuccess, [&]() {
+        login.close();
+        mainwindow.show();
+        });
+    
+    return app.exec();
+}
